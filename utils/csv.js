e9728c56cb89c569ba85ea7c915b7f0b0c01660b
Łukasz Woźniak <lukasz.wozniak.fpl@mailbox.com>
2022-10-31 13:37:19 +0100

    add export to csv file feature

---
+++ utils/csv.js
@@ -0,0 +1,15 @@
+function toPerc(f) {
+    return Math.round(f*1000)/10
+}
+
+export default function objectToCSV(data) {
+    const csv = data.reduce((row,p,i) => {
+        if(i === 0) row += 'id,player_name,leagues,md,odds,xG,variance,variability_sd,variability %,absolute_variability\n'
+        const leagues = p.leagues.sort().join(',')
+        row += p.id + ',' + p.player_name + ',' + '\"' + leagues + '\"' + ',' + p.scores.md + ','
+            + p.scores.odds + ',' + p.scores['xG'] + ',' + toPerc(p.scores.variance) + ','
+            + p.scores.variability.sd + ',' + toPerc(p.scores.variability.perc) + ',' + p.scores.variability.abs_sd + '\n'
+        return row
+    },'')
+    return csv
+}
